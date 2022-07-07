c5fde5cefca337433c6bea0fd994c1a73c4b10f6
Aiko Tan <aikotan.20@ichat.sp.edu.sg>
2022-07-07 15:00:05 +0800

    modify app.js

---
+++ react-app-example/src/App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import './App.css';
+
+class App extends React.Component
+{
+    constructor() {
+        super()
+    }
+
+    render()
+    {
+        return (
+            <div>
+                <h1>Hello React!</h1>
+            </div>
+        );
+    }
+}
+
+export default App;
